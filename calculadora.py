216b8ddc6482fdf69d8940b92576ee7103f5ae93
flamewalker103-commits <ouchalesho@hotmail.com>
2025-09-05 13:16:25 -0500

    feat: initial commit for calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+class Calculadora:
+    def suma(self, a, b):
+        return a + b
+
+    def resta(self, a, b):
+        return a - b
+
+    def multiplicacion(self, a, b):
+        return a * b
+
+    def division(self, a, b):
+        if b == 0:
+            raise ValueError('No se puede dividir entre cero')
+        return a / b
